bfcaea8aa96c97126eaf89036047fc37734049c4
Ferris Thomas <fsthomas@gmail.com>
2017-08-07 13:47:38 -0400

    Some whitespace reformatting. Suppresses emojis from the theme. Updates styles for the Explore page.

--- wp-content/themes/CampSouthernGround/footer.php
+++ wp-content/themes/CampSouthernGround/footer.php
@@ -1,9 +1,9 @@
-  <footer class="clearfix">
+<footer class="clearfix">
     <div class="footer_nav_wrap">
-      <?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'container_class' => 'container' ) ); ?>
+        <?php wp_nav_menu(array('theme_location' => 'footer-menu', 'container_class' => 'container')); ?>
     </div>
-  </footer>
+</footer>
 <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/placeholders.js"></script>
-<?php wp_footer(); ?> 
+<?php wp_footer(); ?>
 </body>
 </html>
--- wp-content/themes/CampSouthernGround/functions.php
+++ wp-content/themes/CampSouthernGround/functions.php
@@ -104,4 +104,7 @@ function highlight_shortcode( $atts, $content = null ) {
 }
 add_shortcode( 'highlight', 'highlight_shortcode' );
 
+// REMOVE EMOJI ICONS
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
 ?>
