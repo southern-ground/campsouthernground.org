41a70c5f6e1c0841205de815837d112b74cac8a3
Ferris Thomas <fsthomas@gmail.com>
2017-09-29 08:44:24 -0400

    Removes placheolders.js script.

--- wp-content/themes/CampSouthernGround/footer.php
+++ wp-content/themes/CampSouthernGround/footer.php
@@ -3,7 +3,7 @@
         <?php wp_nav_menu(array('theme_location' => 'footer-menu', 'container_class' => 'container')); ?>
     </div>
 </footer>
-<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/placeholders.js"></script>
+<!-- <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/placeholders.js"></script> -->
 <?php wp_footer(); ?>
 </body>
 </html>
